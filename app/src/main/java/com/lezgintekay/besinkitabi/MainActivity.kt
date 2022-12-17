5bc9df00ba98b200a982ba13f971371beb7f2a25
Lezgin Tekay <tekay.lezgin@yandex.com>
2022-12-17 19:18:10 +0300

    Navigation and Fragment created

---
+++ app/src/main/java/com/lezgintekay/besinkitabi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lezgintekay.besinkitabi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
