97de5fec29bbbe5b5d7eab3607a1d1bb9fed9202
Lezgin Tekay <tekay.lezgin@yandex.com>
2022-12-29 13:54:12 +0300

    Data Binding

--- app/src/main/java/com/lezgintekay/besinkitabi/adapter/BesinRecyclerAdapter.kt
+++ app/src/main/java/com/lezgintekay/besinkitabi/adapter/BesinRecyclerAdapter.kt
@@ -3,24 +3,27 @@ package com.lezgintekay.besinkitabi.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.lezgintekay.besinkitabi.R
+import com.lezgintekay.besinkitabi.databinding.BesinRecyclerRowBinding
 import com.lezgintekay.besinkitabi.model.Besin
 import com.lezgintekay.besinkitabi.util.gorselIndir
 import com.lezgintekay.besinkitabi.util.placeholderYap
 import com.lezgintekay.besinkitabi.view.BesinListesiFragmentDirections
 import kotlinx.android.synthetic.main.besin_recycler_row.view.*
 
-class BesinRecyclerAdapter(val besinListesi:ArrayList<Besin>) : RecyclerView.Adapter<BesinRecyclerAdapter.BesinViewHolder>() {
+class BesinRecyclerAdapter(val besinListesi:ArrayList<Besin>) : RecyclerView.Adapter<BesinRecyclerAdapter.BesinViewHolder>(),BesinClickListener {
 
-    class BesinViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class BesinViewHolder(var view : BesinRecyclerRowBinding) : RecyclerView.ViewHolder(view.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BesinViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.besin_recycler_row,parent,false)
+        //val view = inflater.inflate(R.layout.besin_recycler_row,parent,false)
+        val view = DataBindingUtil.inflate<BesinRecyclerRowBinding>(inflater, R.layout.besin_recycler_row,parent,false)
         return BesinViewHolder(view)
     }
 
@@ -29,10 +32,10 @@ class BesinRecyclerAdapter(val besinListesi:ArrayList<Besin>) : RecyclerView.Ada
     }
 
     override fun onBindViewHolder(holder: BesinViewHolder, position: Int) {
-
+/*
         holder.itemView.isim.text = besinListesi.get(position).besinIsim
         holder.itemView.kalori.text = besinListesi.get(position).besinKalori
-        //gorsel kısmı eklenecek
+
 
         holder.itemView.setOnClickListener {
             val action = BesinListesiFragmentDirections.actionBesinListesiFragmentToBesinDetayiFragment(besinListesi.get(position).uuid)
@@ -42,12 +45,25 @@ class BesinRecyclerAdapter(val besinListesi:ArrayList<Besin>) : RecyclerView.Ada
         holder.itemView.imageView.gorselIndir(besinListesi.get(position).besinGorsel,
             placeholderYap(holder.itemView.context)
         )
+*/
+        holder.view.besin = besinListesi[position]
+        holder.view.listener=this
+
 
     }
     fun besinListesiniGuncelle(yeniBesinListesi:List<Besin>){
         besinListesi.clear()
         besinListesi.addAll(yeniBesinListesi)
         notifyDataSetChanged()
+    }
+
+    override fun besinTiklandi(view: View) {
+        val uuid = view.besin_uuid.text.toString().toIntOrNull()
+        uuid?.let {
+            val action = BesinListesiFragmentDirections.actionBesinListesiFragmentToBesinDetayiFragment(it)
+            Navigation.findNavController(view).navigate(action)
+        }
+
     }
 
 }
--- app/src/main/java/com/lezgintekay/besinkitabi/view/BesinDetayiFragment.kt
+++ app/src/main/java/com/lezgintekay/besinkitabi/view/BesinDetayiFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lezgintekay.besinkitabi.R
+import com.lezgintekay.besinkitabi.databinding.FragmentBesinDetayiBinding
 import com.lezgintekay.besinkitabi.util.gorselIndir
 import com.lezgintekay.besinkitabi.util.placeholderYap
 import com.lezgintekay.besinkitabi.viewmodel.BesinDetayiViewModel
 import kotlinx.android.synthetic.main.fragment_besin_detayi.*
 
-
+private lateinit var databinding :FragmentBesinDetayiBinding
 private lateinit var viewModel:BesinDetayiViewModel
 private var besinId = 0
 
@@ -30,7 +32,8 @@ class BesinDetayiFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_besin_detayi, container, false)
+        databinding = DataBindingUtil.inflate(inflater,R.layout.fragment_besin_detayi,container,false)
+        return databinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,6 +51,8 @@ class BesinDetayiFragment : Fragment() {
     fun observeLiveData(){
         viewModel.besinLiveData.observe(viewLifecycleOwner, Observer { besin->
             besin?.let {
+               databinding.secilenBesin =it
+                /*
                 besinIsim.text = it.besinIsim
                 besinKalori.text = it.besinKalori
                 besinKarbonhidrat.text = it.besinKarbonhidrat
@@ -56,7 +61,7 @@ class BesinDetayiFragment : Fragment() {
                 context?.let {
                     besinImage.gorselIndir(besin.besinGorsel, placeholderYap(it))
                 }
-
+                */
             }
         })
     }
